97cb62ea8bb428fbeb43b8f403e57ffe2f304e46
Glasc <63518997+Glasc@users.noreply.github.com>
2024-02-01 16:43:11 -0600

    Update auth-app-heading.vue to use NuxtLink instead of anchor tag

--- components/auth/auth-app-heading.vue
+++ components/auth/auth-app-heading.vue
@@ -1,6 +1,7 @@
 <template>
-  <a href="#" class="flex items-center mb-6 text-2xl font-semibold flex-col">
+
+  <NuxtLink to="/" class="flex items-center mb-6 text-2xl font-semibold flex-col">
     <img class="w-10 h-10 mr-2 relative" src="/tomato.png" alt="logo">
     <slot></slot>
-  </a>
+  </NuxtLink>
 </template>
